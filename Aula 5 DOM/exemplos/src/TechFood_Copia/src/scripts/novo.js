71265fdf82e609d54264f846f752d5b28bfe4b8f
victoria-marques <victoria.marques6@aluno.senai.br>
2026-03-05 15:46:42 -0300

    Aula 5

---
+++ Aula 5 DOM/exemplos/src/TechFood_Copia/src/scripts/novo.js
@@ -0,0 +1,11 @@
+const tituloNhoque = document.querySelector('#card-nhoque h3')
+
+const botoesCompra = document.querySelectorAll('.bt_pedido')
+
+const terceiroCard = document.querySelector('.card:nth-child(3)')
+
+console.log("1. Mostrando o título Nhoque (Pelo ID)", tituloNhoque)
+
+console.log("2. Quantidade de botões de pedido", botoesCompra.length)
+
+console.log("3. A terceiro.card de uma class", terceiroCard) 
