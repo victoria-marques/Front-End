8357c1d319e9b87c65135e397912303087298fe2
victoria-marques <victoria.marques6@aluno.senai.br>
2026-03-05 16:00:40 -0300

    DOM

--- Aula 5 DOM/exemplos/src/TechFood_Copia/src/scripts/novo.js
+++ Aula 5 DOM/exemplos/src/TechFood_Copia/src/scripts/novo.js
@@ -9,3 +9,13 @@ console.log("1. Mostrando o título Nhoque (Pelo ID)", tituloNhoque)
 console.log("2. Quantidade de botões de pedido", botoesCompra.length)
 
 console.log("3. A terceiro.card de uma class", terceiroCard) 
+
+const data = new Date()
+const hora = data.getHours()
+
+const saudacao = document.querySelector('#Boas-vindas')
+const seuNome = document.querySelector('#nome')
+
+saudacao.textContent = hora < 18 ? "Bem Vindo, Boa Tarde!" : "Bem Vindo, Boa Noite!"
+
+seuNome.innerHTML = "Meu <strong>nome</strong> é <em>Celso</em>"
